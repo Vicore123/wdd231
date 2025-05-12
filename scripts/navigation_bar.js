6b19cc55907eb9f139b0536bcccb48fc3d9a18b0
Vicore123 <viniciuslimaalves123@gmail.com>
2025-05-12 17:06:50 -0300

    adding animations to the small view menu

--- scripts/navigation_bar.js
+++ scripts/navigation_bar.js
@@ -2,5 +2,5 @@ const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 
 menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
+  menu.classList.toggle('open');
 });
